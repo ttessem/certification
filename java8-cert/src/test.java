2f05f871fd406ecd4f096e97c639c7906a132f4b
BEKKNO\torbjorntessem <torbjorn.tessem@bekk.no>
2018-04-12 11:34:06 +0200

    trufflehog

--- java8-cert/src/test.java
+++ java8-cert/src/test.java
@@ -1,4 +1,7 @@
-
+//test truffleHog
 public class test {
-
+    private final String test1 = "-----BEGIN RSA PRIVATE KEY-----"; //RSA
+    private final String test2 = "01010112345"; //Personnr
+    private final String facebookAuth = "facebook.12rdf342f425f"; //facebook
+    private final String generic = "sEcrEt.asd213wertwwert"; //generic
 }
